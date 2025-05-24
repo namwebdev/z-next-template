cd2ab4f0f2229c0d325ce704fa28966f8c88b104
@jvit-phuongnam <tranphuongnam@jv-it.com.vn>
2025-05-25 05:16:10 +0700

    - Add await for cookie

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -9,7 +9,7 @@ export async function GET(req: Request) {
 
   if (!code) return NextResponse.redirect(`${origin}/${ROUTES.authError}`);
 
-  const cookie = cookies();
+  const cookie = await cookies();
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
